51a86d2ff7e14cc91ecda928d530152bd97acee9
Thomas Schwarz <thomasschwarz96@web.de>
2019-10-22 18:46:16 +0200

    Added forkme svg

--- application/views/components/forkme.php
+++ application/views/components/forkme.php
@@ -1,7 +1,6 @@
 <a id="forkme" href="https://github.com/thomasschwarz96/imageoptimize" target="_blank">
 
-  <img width="149" height="149"
-  src="https://github.blog/wp-content/uploads/2008/12/forkme_right_orange_ff7600.png?resize=149%2C149"
+  <img width="149" height="149" src="<?php echo base_url('assets/application/images/forkme.svg'); ?>"
   class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" />
 
 </a>
