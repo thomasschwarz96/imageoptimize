4adc7183426563d69fce2a353130cf4991072104
Thomas Schwarz <thomasschwarz96@web.de>
2019-10-22 19:23:59 +0200

    UI fixes

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -3,19 +3,19 @@
   <div class="container">
     <div class="row">
 
-      <div class="col-4 text-center">
+      <div class="col-xs-12 col-md-4 text-center">
         <span class="text-muted">
           Page rendered in <strong>{elapsed_time}</strong> seconds.
         </span>
       </div>
 
-      <div class="col-4 text-center">
+      <div class="col-xs-12 col-md-4 text-center">
         <span class="text-muted">
           Images optimized: <strong><?php echo $optimizedImages; ?></strong>
         </span>
       </div>
 
-      <div class="col-4 text-center">
+      <div class="col-xs-12 col-md-4 text-center">
         <span class="text-muted">
           Size optimized: <strong><?php echo $optimizedSize; ?> GB</strong>
         </span>
